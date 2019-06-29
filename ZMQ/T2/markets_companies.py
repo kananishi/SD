6a49343cb9af53a1fba16b5d7fb0b7f3355c39bd
Guilherme Nishi Kanashiro <kanashiro.nishi@gmail.com>
2019-06-29 01:04:42 -0300

    Update markets_companies.py

--- ZMQ/T2/markets_companies.py
+++ ZMQ/T2/markets_companies.py
@@ -1,9 +1,12 @@
 # Lista dos mercados dos principais mercados de acoes
-markets = ["New York Stock Exchange"
-            , "NASDAQ"
-            # , "Japan Exchange Group"
-           # "Shanghai Stock Exchange", "Hong Kong Stock Exchange", "Euronext",
-           # "London Stock Exchange Group", "Shenzhen Stock Exchange"]
+markets = ["New York Stock Exchange",
+           "NASDAQ",
+           "Japan Exchange Group",
+           "Shanghai Stock Exchange",
+           "Hong Kong Stock Exchange", 
+           "Euronext",
+           "London Stock Exchange Group",
+           "Shenzhen Stock Exchange"]
            ]
 
 # Lista de acoes de cada mercado
@@ -13,11 +16,22 @@ companies = {"New York Stock Exchange" : ["BABA", "V", "JNJ", "XOM", "WMT", "PG"
                                         "NVS", "TSM"],
              "NASDAQ" : ["MSFT", "AMZN", "AAPL", "GOOGL", "GOOG", "FB", "CSCO",
                        "INTC", "CMCSA", "PEP", "NFLX", "ADBE", "PYPL", "COST",
-                       "AVGO", "AMGN", "TXN", "SBUX", "NVDA", "CHTR"]
-              # "Japan Exchange Group" : [],
-              # "Shanghai Stock Exchange" : [],
-              # "Hong Kong Stock Exchange" : [],
-              # "Euronext" : [],
-              # "London Stock Exchange Group" : [],
-              # "Shenzhen Stock Exchange" : []
+                       "AVGO", "AMGN", "TXN", "SBUX", "NVDA", "CHTR"],
+              "Japan Exchange Group" : [],
+              "Shanghai Stock Exchange" : ["SPD BANK", "DFAC", "CWTC","BEIJING CAPITAL",
+                                           "SIA","BSU","HPI","Anhui Expressway","HUAXIA BANK",
+                                           "CMBC","RIZHAO PORT","SIPG","Baosteel","Zhongyuan Expressway",
+                                           "SEP", "SHANDONG STEEL","Zhejiang Energy Electric Power",
+                                           "Huaneng Hydropower ", "CSET","HDPI"],
+              "Hong Kong Stock Exchange" : [],
+              "Euronext" : ["ALMIL","2CRSI","MLATV","ASP","AALB,","ABI","AB","DOY","ABCA","ABEO","ABVX",
+                            "ABN","ABO","MLGAI","GALIM","OR","LFVE","LBON","MLPGO","PSH","ZENT"],
+              "London Stock Exchange Group" : ["MERL","CLG","NOG","MGP","CARR",
+                                               "INDV","STOB","IAG","EZJ","SNR",
+                                              "COST","IPF","NANO","ALFA","PAY",
+                                               "KIE","MPO","FCH","PFC","TPT"],
+              "Shenzhen Stock Exchange" : ["NHU","JSQH","WEIXING","HUAPONT PHARM","ETI","JINGGONG SCIENCE",
+                                           "HUALAN BIO.","HAN'S LASER","MIRACLE LOGISTICS","KAN",
+                                           "HAPM","NOVEL","XIAKE","SRZY","ZOJE","KHB","HAITE",
+                                           "SUPOR","LIJIANG TOURISM"]
               }
